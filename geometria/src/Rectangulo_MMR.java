9f623976f0eff328fa4c3943936cea57b54e708b
Mikel Mendoza <mikelmendozarubio89@gmail.com>
2019-02-13 20:25:15 +0100

    versión comentada

--- geometria/src/Rectangulo_MMR.java
+++ geometria/src/Rectangulo_MMR.java
@@ -1,19 +1,33 @@
-
+/**
+ * 
+ * @author Mikel
+ * Mikel Mendoza Rubio
+ * Versión 1.2
+ */
 public class Rectangulo_MMR extends FiguraGeometrica_MMR {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * Constructor
+	 * @param  tipoFigura 
+	 * @param  lG 
+	 * @param  lP
+	 */
 	public Rectangulo_MMR(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+    /**
+     * Mide el area
+     */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Mide el perimetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
