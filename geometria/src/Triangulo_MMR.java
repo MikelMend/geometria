28320653ee50a9e2a3d118deeaa24ff4304c9a83
Mikel Mendoza <mikelmendozarubio89@gmail.com>
2019-02-13 20:17:30 +0100

    nuevo método

--- geometria/src/Triangulo_MMR.java
+++ geometria/src/Triangulo_MMR.java
@@ -22,8 +22,5 @@ public class Triangulo_MMR extends FiguraGeometrica_MMR {
 		sp = this.semiPerimetro();
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+
 }
